352c016582cbb539e9455a83a91b139cd8fe92ac
Mansi agarwal <mansiagarwal293@gmail.com>
2025-01-29 15:36:38 +0530

    router update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 import AuthContextProvider from "./AuthContextProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthContextProvider>
       <App />
     </AuthContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
